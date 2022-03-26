91666e0a1b04b8a737b14a89c3599e433d36b505
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-03-26 05:05:39 -1200

    calculation of average length & orientation of bacteria were added.

--- FAST/Mono Culture/post-processing/scripts/FASTAnalysis.m
+++ FAST/Mono Culture/post-processing/scripts/FASTAnalysis.m
@@ -10,16 +10,17 @@ birthLength=[];
 lifeHistory=[];
 velocity=[];
 AverageVelocity=[];
-number_of_cells_in_each_timesteps=[];
-
-Number_of_timesteps=max(cell2mat({procTracks.end}));
-cellLifeHistory=transpose([procTracks.start;procTracks.end]);
+AverageLength=[];
+AverageOrientation=[];
 
 for i=1:cellNumber
     %bacteri length
     birthLengthValue= procTracks(i).majorLen(1);
     lastLengthValue= procTracks(i).majorLen(end);
     lifeHistoryValue=procTracks(i).length;
+    meanLength=mean(procTracks(i).majorLen);
+    %radian
+    meanOrientation=mean(procTracks(i).phi);
 
     if lifeHistoryValue >= minLife
         growth_rate_value=(log(lastLengthValue)-log(birthLengthValue))/(lifeHistoryValue*intervalTime);
@@ -39,25 +40,15 @@ for i=1:cellNumber
     birthLength(end+1)=birthLengthValue;
     lifeHistory(end+1)=lifeHistoryValue;
     AverageVelocity(end+1)=AverageVelocityValue;
+    AverageLength(end+1)=meanLength;
+    AverageOrientation(end+1)=meanOrientation;
 end
 
 
-%number of cell per time steps
-for j=1:Number_of_timesteps
-    number_of_cells=length(cellLifeHistory(j>=cellLifeHistory(:,1) & j<= cellLifeHistory(:,2)));
-    number_of_cells_in_each_timesteps(end+1)=number_of_cells;
-end    
-
-
-
 %add to table
-T = table(transpose(1:length(growth_rate)),transpose(growth_rate),transpose(birthLength),transpose(lifeHistory),transpose(AverageVelocity));
-T2=table(transpose(1:length(number_of_cells_in_each_timesteps)),transpose(number_of_cells_in_each_timesteps));
+T = table(transpose(1:length(growth_rate)),transpose(growth_rate),transpose(birthLength),transpose(lifeHistory),transpose(AverageVelocity),transpose(AverageLength),transpose(AverageOrientation));
 %add column name
-T.Properties.VariableNames={'CellNumber','GrowthRate','BirthLength','LifeHistory','AverageVelocity'};
-T2.Properties.VariableNames={'StepNumber','NumberOfCells'};
+T.Properties.VariableNames={'CellNumber','GrowthRate','BirthLength','LifeHistory','AverageVelocity','AverageLength','AverageOrientation'};
 
 % write to csv
 writetable(T,'../results/FASTAnalysis.csv','Delimiter',',','QuoteStrings',true)
-writetable(T2,'../results/FAST_TimeSteps_Analysis.csv','Delimiter',',','QuoteStrings',true)
-
