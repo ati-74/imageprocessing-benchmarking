e7f92b7de02ab36eef7e1b49acf0db58f64d32d5
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-03-30 05:47:31 -1200

    Update ProcessCellProfilerData.py

--- CellProfiler/Mono Culture/post-processing/scripts/ProcessCellProfilerData.py
+++ CellProfiler/Mono Culture/post-processing/scripts/ProcessCellProfilerData.py
@@ -5,25 +5,28 @@ import os
 from ExperimentalDataProcessing import BacteriaAnalysis
 
 
-def ProcessData(input_file,interval_time=1,growth_rate_method="Average"):
+def ProcessData(input_file, interval_time=1, growth_rate_method="Average"):
 
-    #Parsing CellProfiler output
-    dataFrame=pd.read_csv(input_file)
-    #remove Nan lables and zero MajorAxisLength
-    dataFrame=dataFrame.loc[(dataFrame["TrackObjects_Label_50"]!='nan') &
-                                  (dataFrame["AreaShape_MajorAxisLength"] != 0)]
-    dataFrame=dataFrame.reset_index(drop=True)
-        
-    #process the tracking data
-    processed_data=BacteriaAnalysis(dataFrame,interval_time,growth_rate_method)
+    # Parsing CellProfiler output
+    dataFrame = pd.read_csv(input_file)
+    # remove Nan lables and zero MajorAxisLength
+    dataFrame = dataFrame.loc[
+        (dataFrame["TrackObjects_Label_50"] != "nan")
+        & (dataFrame["AreaShape_MajorAxisLength"] != 0)
+    ]
+    dataFrame = dataFrame.reset_index(drop=True)
 
-    #write to csv
-    path="../results/CPAnalysis"
-    processed_data.to_csv(path+'.csv', index=False)
+    # process the tracking data
+    processed_data = BacteriaAnalysis(dataFrame, interval_time, growth_rate_method)
+
+    # write to csv
+    path = "../results/CPAnalysis"
+    processed_data.to_csv(path + ".csv", index=False)
 
 
-
-    
-input_file="../../4. CP outputs/MyExpt_IdentifySecondaryObjects - remove - Nan Lable.csv"
-interval_time=1.5
-ProcessData(input_file,interval_time)
+if __name__ == "__main__":
+    input_file = (
+        "../../4. CP outputs/MyExpt_IdentifySecondaryObjects - remove - Nan Lable.csv"
+    )
+    interval_time = 1.5
+    ProcessData(input_file, interval_time)
